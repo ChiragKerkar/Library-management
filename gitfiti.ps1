95caebf0c48c26ce07adb350e763c9d4177b431c
chirag <kerkarc509@gmail.com>
2025-05-11 12:18:55 +0530

    Add Gitfiti script and art commits

---
+++ gitfiti.ps1
@@ -0,0 +1,421 @@
+cd $PSScriptRoot
+$REPO="Library-management"
+git init $REPO
+cd $REPO
+New-Item README.md -ItemType file | Out-Null
+git add README.md
+New-Item gitfiti -ItemType file | Out-Null
+git add gitfiti
+$Env:GIT_AUTHOR_DATE="2024-11-10T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-10T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-10T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-10T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-10T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-10T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-10T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-10T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-11T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-11T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-11T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-11T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-11T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-11T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-11T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-11T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-12T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-12T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-12T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-12T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-12T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-12T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-12T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-12T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-13T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-13T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-13T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-13T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-13T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-13T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-13T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-13T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-14T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-14T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-14T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-14T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-14T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-14T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-14T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-14T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-16T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-16T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-16T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-16T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-16T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-16T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-16T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-16T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-17T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-17T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-17T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-17T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-17T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-17T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-17T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-17T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-18T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-18T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-18T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-18T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-18T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-18T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-19T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-19T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-20T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-20T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-20T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-20T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-20T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-20T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-21T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-21T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-21T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-21T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-21T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-21T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-21T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-21T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-23T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-23T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-23T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-23T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-23T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-23T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-23T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-23T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-24T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-24T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-24T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-24T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-24T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-24T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-24T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-24T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-25T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-25T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-25T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-25T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-25T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-25T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-26T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-26T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-26T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-26T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-26T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-26T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-27T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-27T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-27T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-27T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-27T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-27T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-28T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-28T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-28T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-28T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-28T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-28T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-28T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-28T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-29T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-29T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-29T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-29T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-29T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-29T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-29T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-29T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-30T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-30T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-30T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-30T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-30T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-30T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-11-30T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-11-30T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-01T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-01T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-01T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-01T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-01T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-01T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-01T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-01T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-02T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-02T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-02T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-02T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-02T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-02T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-03T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-03T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-03T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-03T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-03T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-03T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-04T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-04T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-04T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-04T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-04T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-04T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-05T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-05T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-05T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-05T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-05T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-05T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-05T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-05T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-06T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-06T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-06T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-06T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-06T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-06T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-06T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-06T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-07T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-07T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-07T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-07T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-07T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-07T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-07T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-07T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-08T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-08T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-08T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-08T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-08T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-08T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-08T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-08T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-09T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-09T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-09T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-09T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-09T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-09T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-10T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-10T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-11T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-11T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-11T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-11T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-11T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-11T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-12T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-12T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-12T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-12T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-12T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-12T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-12T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-12T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-14T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-14T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-14T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-14T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-14T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-14T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-14T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-14T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-15T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-15T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-15T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-15T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-15T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-15T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-15T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-15T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-16T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-16T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-16T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-16T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-16T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-16T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-16T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-16T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-17T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-17T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-17T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-17T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-17T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-17T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-17T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-17T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-18T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-18T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-18T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-18T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-18T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-18T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-18T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-18T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-19T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-19T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-19T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-19T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-19T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-19T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-19T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-19T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-21T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-21T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-21T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-21T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-21T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-21T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2024-12-21T12:00:00"
+$Env:GIT_COMMITTER_DATE="2024-12-21T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+
+git branch -M main
+git remote add origin git@github.com:ChiragKerkar/$REPO.git
+git pull origin main
+git push -u origin main
